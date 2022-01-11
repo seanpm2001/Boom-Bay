4bbeefbb2c5c8f728f3fc293faf66392faadcf81
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-11 13:51:19 -0800

    VARIABLES.scm (V1)
    Version 1 of the variables file for this game.

---
+++ Boom-Bay/SBeach/VARIABLES.scm
@@ -0,0 +1,74 @@
+#!/usr/local/bin/guile \< ;; I believe this is the proper Shebang
+;; Start of script
+;; Sample Scheme source code for reference (Wikipedia: Scheme#Minimalism)
+;; (define-syntax let
+;;  (syntax-rules ()
+;;    ((let ((var expr) ...) body ...)
+;;      ((lambda (var ...) body ...) expr ...))))
+;; I am not very good at the Scheme programming language, so this is just pseudocode for now
+;; Resources
+;; Diamonds
+(let (var expr) exp diamonds startAm(1000) currentAm(10000) max = 1000000000 (())
+;; Gold
+(let (var expr) exp gold startAm(10000) currentAm(10000) max = maxStorageCap.current (())
+;; Wood
+(let (var expr) exp wood startAm(1000) currentAm(1000) max = maxStorageCap.current (())
+;; Stone
+(let (var expr) exp stone startAm(0) currentAm(0) max = maxStorageCap.current (unlock at HQlevel(5) ()))
+;; Iron
+(let (var expr) exp iron startAm(0) currentAm(0) max = maxStorageCap.current (unlock at HQlevel(12) ()))
+;; Crystals
+;; Life Fragment
+(let (var expr) exp crystal.life_fragment startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Life Shard
+(let (var expr) exp crystal.life_shard startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Life Crystal
+(let (var expr) exp crystal.life_crystal startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Fire Fragment
+(let (var expr) exp crystal.fire_fragment startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Fire Shard
+(let (var expr) exp crystal.fire_shard startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Fire Crystal
+(let (var expr) exp crystal.fire_crystal startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Ice Fragment
+(let (var expr) exp crystal.ice_fragment startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Ice Shard
+(let (var expr) exp crystal.ice_shard startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Ice Crystal
+(let (var expr) exp crystal.ice_crystal startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Dark Fragment
+(let (var expr) exp crystal.dark_fragment startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Dark Shard
+(let (var expr) exp crystal.dark_shard startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Dark Crystal
+(let (var expr) exp crystal.dark_crystal startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(3) ()))
+;; Building parts
+;; Gear
+(let (var expr) exp material.gear startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(9) ()))
+;; Flux capacitor
+(let (var expr) exp material.flux_capacitor startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(9) ()))
+;; Field capacitor
+(let (var expr) exp material.field_capacitor startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(9) ()))
+;; Power rod
+(let (var expr) exp material.power_rod startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(9) ()))
+;; Critical fuse
+(let (var expr) exp material.critical_fuse startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(9) ()))
+;; Progress
+;; XP (Experience Points)
+(let (var expr) exp xpStartAm(0) max = 1000000000000000000000()) ;; XP
+;; Level
+(let (var expr) exp levelStartAm(1) max = 10000 ;; Level
+;; Level requirements
+(let (var expr) exp levelReq = level.current + 1 * xp / 8 ()) ;; Level requirements
+;; Medals
+(let (var expr) exp metalStartAm(0) currentAm(0) max = 1000000000 ;; Medals, earn 1-5 per battle, lose 1-3 per defeat
+;; Other
+;; Trader token
+(let (var expr) ttoken startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(4) ()))
+;; Hero token
+(let (var expr) heroTkn startAm(0) currentAm(0) max = 1000000000 (unlock at HQlevel(15) ()))
+;; File info
+;; File type: Scheme source file (*.ss *.scm)
+;; File version: 1 (2022, Tuesday, January 11th at 1:51 pm)
+;; Line count (including blank lines and compiler line): 75
+;; End of script
