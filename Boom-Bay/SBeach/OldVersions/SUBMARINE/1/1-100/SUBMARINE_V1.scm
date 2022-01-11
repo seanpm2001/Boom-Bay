90c79375f7ff28bc1e548f44756c7bdea9fd61ad
Sean P. Myrick V19.1.7.2 <seanwallawalla@protonmail.com>
2022-01-11 13:27:42 -0800

    SUBMARINE_V1.scm
    An archive of the first version of the submarine script for this project.

---
+++ Boom-Bay/SBeach/OldVersions/SUBMARINE/1/1-100/SUBMARINE_V1.scm
@@ -0,0 +1,16 @@
+#!/usr/local/bin/guile \< ;; I believe this is the proper Shebang
+;; Start of script
+;; Sample Scheme source code for reference (Wikipedia: Scheme#Minimalism)
+;; (define-syntax let
+;;  (syntax-rules ()
+;;    ((let ((var expr) ...) body ...)
+;;      ((lambda (var ...) body ...) expr ...))))
+(let (var expr) submarine) body "unlocks at HQ level 7"()
+ (let (var expr) divespot1) body ( ()
+  (let (var ds1_rewards) gold(1000) wood(900) stone(650) iron(400)))
+;; I am going to stop here, I don't know how to work this right now
+;; File info
+;; File type: Scheme source file (*.ss *.scm)
+;; File version: 1 (2022, Tuesday, January 11th at 1:27 pm)
+;; Line count (including blank lines and compiler line): 17
+;; End of script
